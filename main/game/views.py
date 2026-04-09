7165c288d00f0a92537ea0be3b507ce02763ab02
Артём Большаков <artembolsakov500@gmail.com>
2026-04-09 23:57:48 +0300

    sdfnjdfjfbhj

--- main/game/views.py
+++ main/game/views.py
@@ -4,7 +4,6 @@ from django.contrib.auth import login, logout
 from django.conf import settings
 from .forms import RegisterForm, LoginForm
 from .models import GameQuestion, UserProgress, Answer
-import os
 
 
 def register(request):
@@ -14,7 +13,8 @@ def register(request):
             user = form.save()
             login(request, user)
             first_question = GameQuestion.objects.first()
-            UserProgress.objects.create(user=request.user, current_question=first_question)
+            UserProgress.objects.create(user=request.user,
+                                        current_question=first_question)
             return redirect('question')
     else:
         form = RegisterForm()
@@ -23,27 +23,40 @@ def register(request):
 
 @login_required
 def question_view(request):
+    first_question = GameQuestion.objects.filter(is_first=True)[0]
     progress, created = UserProgress.objects.get_or_create(
         user=request.user,
         defaults={
-            'current_question': GameQuestion.objects.first(),
+            'current_question': first_question,
             'emotion_score': 50,
             'time_score': 50
         }
     )
 
     if not progress.current_question:
-        first_question = GameQuestion.objects.first()
-        if first_question:
-            progress.current_question = first_question
-            progress.save()
+        progress.current_question = first_question
+        progress.save()
 
     question = progress.current_question
-
     if request.method == 'POST':
         answer_id = request.POST.get('answer_id')
         answer = get_object_or_404(Answer, id=answer_id)
-
+        if answer.question.text == 'Я так нервничала перед защитой, что совсем не получалось поспать. Может перекусить?' and answer.text == 'Нет':
+            return render(request, 'game/game_over.html', context={'message': 'Вы упали в голодный обморок!'})
+        if answer.question.text == 'Как будет быстрее добраться до кабинета?' and answer.text == 'По левой лестнице':
+            return render(request, 'game/game_over.html', context={'message': 'Вы встретили кого важного, GG'})
+        if answer.text == 'Здорово, а какие вопросы задавали?':
+            history = UserProgress.objects.filter(user=request.user)[0].answers_history
+            target_question = None
+            for item in history:
+                if item['question_text'] == 'Какой макияж выбрать?':
+                    target_question = item
+                    print('TRUE')
+                    break
+            if target_question['answer_text'] != 'Не краситься':
+                print('Хороший ответ')
+            else:
+                print('Bad answer')
         progress.emotion_score = progress.emotion_score + answer.emotion_change
         progress.time_score = progress.time_score + answer.time_change
 
@@ -81,7 +94,7 @@ def question_view(request):
 @login_required
 def game_result(request):
     progress = request.user.game_progress
-    
+
     emotion = progress.emotion_score
     time_score = progress.time_score
 
@@ -105,14 +118,7 @@ def game_result(request):
         ending_title = '😔 Грустный конец'
         ending_text = 'К сожалению, вы не получили удовольствия и потратили время впустую. В следующий раз стоит выбирать иначе!'
         ending_image = 'game/images/bad_ending.jpg'
-    
-    # Сбрасываем прогресс после показа концовки (опционально)
-    # progress.current_question = GameQuestion.objects.first()
-    # progress.emotion_score = 50
-    # progress.time_score = 50
-    # progress.answers_history = []
-    # progress.save()
-    
+
     return render(request, 'game/game_result.html', {
         'ending_title': ending_title,
         'ending_text': ending_text,
@@ -126,7 +132,7 @@ def game_result(request):
 @login_required
 def reset_game(request):
     progress = request.user.game_progress
-    first_question = GameQuestion.objects.first()
+    first_question = GameQuestion.objects.filter(is_first=True)[0]
     progress.current_question = first_question
     progress.answers_history = []
     progress.save()
@@ -136,7 +142,7 @@ def reset_game(request):
 def login_view(request):
     if request.user.is_authenticated:
         return redirect('question')
-    
+
     if request.method == 'POST':
         form = LoginForm(request, data=request.POST)
         if form.is_valid():
@@ -145,7 +151,7 @@ def login_view(request):
             return redirect('question')
     else:
         form = LoginForm()
-    
+
     return render(request, 'game/login.html', {'form': form})
 
 
